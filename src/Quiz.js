8f782bfcef02a3382c65bc150b27bdb16d4362a3
Ajay Kumar Tiwari <ajay11071982@gmail.com>
2018-11-26 12:30:05 +0530

    Update Quiz.js
    quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,11 +18,17 @@ class Quiz extends Component {
             <div>
                 <div className="QuizQuestion">
                     {isQuizEnd ? <QuizEnd/> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/> }
+                    <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/> }
                 </div>
             </div>
         )
     };
+
+    showNextQuestion() {
+        this.setState({
+            quiz_position: this.state.quiz_position + 1
+        });
+    }
 }
 
 export default Quiz
