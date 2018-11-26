613e71f2c8362a8a2291f10faa2f44e80ef72401
Ajay Kumar Tiwari <ajay11071982@gmail.com>
2018-11-26 12:46:17 +0530

    Update QuizQuestion.js
    show error message for wrong answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
+
     render() {
         return (
             <main>
@@ -11,19 +19,26 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_option, index) => {
-                               return <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option}/>
+                                return <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index}
+                                                           button_text={answer_option}/>
                             }
                         )}
 
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ?
+                    <p className='error'>Sorry, that's not right</p>
+                    : null}
             </main>
         )
     }
 
     handleClick(buttonText) {
-        if(this.props.quiz_question.answer === buttonText) {
+        if (this.props.quiz_question.answer === buttonText) {
+            this.setState({incorrectAnswer: false});
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState({incorrectAnswer: true});
         }
     }
 }
