0ece4f20e42705d0a31461d4129f18ffbaba434f
Ajay Kumar Tiwari <ajay11071982@gmail.com>
2018-11-26 12:29:49 +0530

    Update QuizQuestion.js
    quiz questions

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,11 +10,21 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                               return <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option}/>
+                            }
+                        )}
+
                     </ul>
                 </section>
             </main>
         )
+    }
+
+    handleClick(buttonText) {
+        if(this.props.quiz_question.answer === buttonText) {
+            this.props.showNextQuestionHandler();
+        }
     }
 }
 
